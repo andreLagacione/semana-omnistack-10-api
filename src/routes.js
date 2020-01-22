05a38733be279f92ccfdf9ad2dc983ca05d8d594
andreLagacione <lagacioneandre@gmail.com>
2020-01-22 20:52:58 -0300

    separado os metodos para um controller

--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,12 @@
 const { Router } = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev');
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
 
-routes.post('/devs', async (request, response) => {
-    const { github_username, techs, latitude, longitude } = request.body;
-    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-    const { name = login, avatar_url, bio } = apiResponse.data;
-    const techsArray = techs.split(',').map(tech => tech.trim());
-    const location = {
-        type: 'Point',
-        coordinates: [longitude, latitude],
-    }
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
 
-    const dev = await Dev.create({
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs: techsArray,
-        location
-    })
-
-    return response.json(dev);
-});
+routes.post('/search', SearchController.index);
 
 module.exports = routes;
